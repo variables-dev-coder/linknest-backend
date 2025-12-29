054f88699d3880c3fe204e36c4ef14d08f6d39af
Munna Aziz Mondal <azizmunna661@gmail.com>
2025-12-29 15:38:59 +0530

    Day 1: Project setup with Spring Boot and PostgreSQL

---
+++ linknest-api/src/main/java/com/linknest/LinknestApiApplication.java
@@ -0,0 +1,13 @@
+package com.linknest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinknestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinknestApiApplication.class, args);
+	}
+
+}
---
+++ linknest-api/src/test/java/com/linknest/LinknestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linknest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinknestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
